3ff466de2d8e95ee1515dc9cddec311d04cf3cdd
JoachimMorisPXL <148859852+JoachimMorisPXL@users.noreply.github.com>
2026-05-12 16:02:34 +0200

    Add files via upload

---
+++ taak-37.py
@@ -0,0 +1,5 @@
+# Gebruik de RAW URL (zonder 'tree/main' en met raw.githubusercontent.com)
+GITHUB_RAW_URL = (
+    "https://raw.githubusercontent.com/JoachimMorisPXL/"
+    "Code-Router/main/configs/router-config.xml"
+)
